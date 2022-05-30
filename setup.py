74de291b0ccc069405760c166657a3965043de56
Philip Roche <phil.roche@canonical.com>
2022-05-30 14:20:14 +0100

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/canonical/ubuntu_ppa_package_version_report',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
